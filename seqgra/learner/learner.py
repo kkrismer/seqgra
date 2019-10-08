9e01f6a824b5b8f995c927e08ca120dbfe58b0ca
Konstantin Krismer <krismer@mit.edu>
2019-10-08 17:43:04 -0400

    Add base class constructors

--- seqgra/learner/learner.py
+++ seqgra/learner/learner.py
@@ -13,95 +13,75 @@ MIT - CSAIL - Gifford Lab - seqgra
 
 @author: Konstantin Krismer
 """
+import os
 
 from abc import ABC, abstractmethod
 from typing import List
 
 class Learner(ABC):
     @abstractmethod
-    def parse_data(self, x_train: List[str], y_train: List[str], x_val: List[str], y_val: List[str]) -> None:
-        """ This is the forward calculation from x to y
-        Returns:
-            softmax_linear: Output tensor with the computed logits.
-        """
+    def __init__(self, output_dir: str) -> None:
+        self.output_dir = output_dir
+        self.__prepare_output_dir()
+
+    @abstractmethod
+    def parse_data(self, training_file: str, validation_file: str) -> None:
         pass
 
     @abstractmethod
     def predict(self):
-        """ This is the forward calculation from x to y
-        Returns:
-            softmax_linear: Output tensor with the computed logits.
-        """
         pass
 
     @abstractmethod
     def train_model(self):
-        """Sets up the training Ops.
-        Creates a summarizer to track the loss over time in TensorBoard.
-        Creates an optimizer and applies the gradients to all trainable variables.
-        The Op returned by this function is what must be passed to the
-        `sess.run()` call to cause the model to train.
-        Returns:
-            train_op: The Op for training.
-        """
         pass
 
     @abstractmethod
     def save_model(self, model_name: str):
         pass
 
+    def __prepare_output_dir(self) -> None:
+        if os.path.exists(self.output_dir):
+            if os.path.isdir(self.output_dir):
+                if len(os.listdir(self.output_dir)) > 0:
+                    raise Exception("output directory non-empty")
+            else:
+                raise Exception("output directory cannot be created (file with same name exists)")
+        else:    
+            os.makedirs(self.output_dir)
+
 class MultiClassClassificationLearner(Learner):
     @abstractmethod
+    def __init__(self, output_dir: str) -> None:
+        super().__init__(output_dir)
+
+    @abstractmethod
     def evaluate_model(self, x: List[str], y: List[str]):
-        """Evaluate the quality of the logits at predicting the label.
-        Args:
-            logits: Logits tensor, float - [batch_size, NUM_CLASSES].
-            labels: Labels tensor, int32 - [batch_size], with values in the
-            range [0, NUM_CLASSES).
-        Returns:
-            A scalar int32 tensor with the number of examples (out of batch_size)
-            that were predicted correctly.
-        """
         pass
 
 class MultiLabelClassificationLearner(Learner):
     @abstractmethod
+    def __init__(self, output_dir: str) -> None:
+        super().__init__(output_dir)
+
+    @abstractmethod
     def evaluate_model(self, x: List[str], y: List[List[str]]):
-        """Evaluate the quality of the logits at predicting the label.
-        Args:
-            logits: Logits tensor, float - [batch_size, NUM_CLASSES].
-            labels: Labels tensor, int32 - [batch_size], with values in the
-            range [0, NUM_CLASSES).
-        Returns:
-            A scalar int32 tensor with the number of examples (out of batch_size)
-            that were predicted correctly.
-        """
         pass
 
 class MultipleRegressionLearner(Learner):
     @abstractmethod
+    def __init__(self, output_dir: str) -> None:
+        super().__init__(output_dir)
+
+    @abstractmethod
     def evaluate_model(self, x: List[str], y: List[float]):
-        """Evaluate the quality of the logits at predicting the label.
-        Args:
-            logits: Logits tensor, float - [batch_size, NUM_CLASSES].
-            labels: Labels tensor, int32 - [batch_size], with values in the
-            range [0, NUM_CLASSES).
-        Returns:
-            A scalar int32 tensor with the number of examples (out of batch_size)
-            that were predicted correctly.
-        """
         pass
 
 class MultivariateRegressionLearner(Learner):
     @abstractmethod
+    def __init__(self, output_dir: str) -> None:
+        super().__init__(output_dir)
+
+    @abstractmethod
     def evaluate_model(self, x: List[str], y: List[List[float]]):
-        """Evaluate the quality of the logits at predicting the label.
-        Args:
-            logits: Logits tensor, float - [batch_size, NUM_CLASSES].
-            labels: Labels tensor, int32 - [batch_size], with values in the
-            range [0, NUM_CLASSES).
-        Returns:
-            A scalar int32 tensor with the number of examples (out of batch_size)
-            that were predicted correctly.
-        """
         pass
