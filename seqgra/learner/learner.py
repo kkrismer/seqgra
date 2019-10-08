a5422398c14981ff58af9b5bc6c7063add726540
Konstantin Krismer <krismer@mit.edu>
2019-10-08 15:26:11 -0400

    Add learners for classification and regression

--- seqgra/learner/learner.py
+++ seqgra/learner/learner.py
@@ -1,16 +1,25 @@
 """
 MIT - CSAIL - Gifford Lab - seqgra
 
-Abstract base class for learners
+- abstract base class for all learners
+- abstract class for multi-class classification learners, i.e., learners
+  for data where the class labels are mututally exclusive
+- abstract class for multi-label classification learners, i.e., learners
+  for data where the class labels are not mututally exclusive
+- abstract class for multiple regression learners, i.e., learners with 
+  multiple independent variables and one dependent variable
+- abstract class for multivariate regression learners, i.e., learners with 
+  multiple independent variables and multiple dependent variables
 
 @author: Konstantin Krismer
 """
 
 from abc import ABC, abstractmethod
+from typing import List
 
 class Learner(ABC):
     @abstractmethod
-    def parse_data(self, x_train, y_train, x_val, y_val, x_test, y_test) -> None:
+    def parse_data(self, x_train: List[str], y_train: List[str], x_val: List[str], y_val: List[str]) -> None:
         """ This is the forward calculation from x to y
         Returns:
             softmax_linear: Output tensor with the computed logits.
@@ -38,7 +47,54 @@ class Learner(ABC):
         pass
 
     @abstractmethod
-    def evaluate_model(self):
+    def save_model(self, model_name: str):
+        pass
+
+class MultiClassClassificationLearner(Learner):
+    @abstractmethod
+    def evaluate_model(self, x: List[str], y: List[str]):
+        """Evaluate the quality of the logits at predicting the label.
+        Args:
+            logits: Logits tensor, float - [batch_size, NUM_CLASSES].
+            labels: Labels tensor, int32 - [batch_size], with values in the
+            range [0, NUM_CLASSES).
+        Returns:
+            A scalar int32 tensor with the number of examples (out of batch_size)
+            that were predicted correctly.
+        """
+        pass
+
+class MultiLabelClassificationLearner(Learner):
+    @abstractmethod
+    def evaluate_model(self, x: List[str], y: List[List[str]]):
+        """Evaluate the quality of the logits at predicting the label.
+        Args:
+            logits: Logits tensor, float - [batch_size, NUM_CLASSES].
+            labels: Labels tensor, int32 - [batch_size], with values in the
+            range [0, NUM_CLASSES).
+        Returns:
+            A scalar int32 tensor with the number of examples (out of batch_size)
+            that were predicted correctly.
+        """
+        pass
+
+class MultipleRegressionLearner(Learner):
+    @abstractmethod
+    def evaluate_model(self, x: List[str], y: List[float]):
+        """Evaluate the quality of the logits at predicting the label.
+        Args:
+            logits: Logits tensor, float - [batch_size, NUM_CLASSES].
+            labels: Labels tensor, int32 - [batch_size], with values in the
+            range [0, NUM_CLASSES).
+        Returns:
+            A scalar int32 tensor with the number of examples (out of batch_size)
+            that were predicted correctly.
+        """
+        pass
+
+class MultivariateRegressionLearner(Learner):
+    @abstractmethod
+    def evaluate_model(self, x: List[str], y: List[List[float]]):
         """Evaluate the quality of the logits at predicting the label.
         Args:
             logits: Logits tensor, float - [batch_size, NUM_CLASSES].
