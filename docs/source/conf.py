1449572ddd4ff039fefee3f09439a2eb79f7b2c9
Konstantin Krismer <krismer@mit.edu>
2020-04-19 21:03:19 -0400

    Fix sphinx config

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,30 +10,41 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('../../'))
+import os
+import sys
+import seqgra
+
+sys.path.insert(0, os.path.abspath("../../seqgra/"))
 
 
 # -- Project information -----------------------------------------------------
 
-project = 'seqgra'
-copyright = '2020, Konstantin Krismer'
-author = 'Konstantin Krismer'
+project = "seqgra"
+copyright = "2020, Konstantin Krismer"
+author = "Konstantin Krismer"
 
+version = seqgra.__version__
 # The full version, including alpha/beta/rc tags
-release = '0.0.1'
+release = seqgra.__version__
 
 
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# extensions coming with Sphinx (named "sphinx.ext.*") or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc']
+extensions = ["sphinx.ext.autodoc",
+              "sphinx.ext.napoleon",
+              "sphinx.ext.viewcode",
+              "sphinx.ext.autosummary",
+              "sphinxcontrib.apidoc"]
+
+apidoc_module_dir = "../../seqgra"
+apidoc_output_dir = "."
+apidoc_extra_args = ["--implicit-namespaces"]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -46,9 +57,9 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
