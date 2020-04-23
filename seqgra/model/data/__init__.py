27316751fbcf2643c861f0ec2120fa77c038c894
Konstantin Krismer <krismer@mit.edu>
2020-04-23 02:46:23 -0400

    Fix data model classes imports order

--- seqgra/model/data/__init__.py
+++ seqgra/model/data/__init__.py
@@ -1,11 +1,11 @@
-from seqgra.model.data.alphabetdistribution import AlphabetDistribution
-from seqgra.model.data.background import Background
-from seqgra.model.data.condition import Condition
-from seqgra.model.data.datageneration import DataGeneration
-from seqgra.model.data.datageneration import ExampleSet, Example
-from seqgra.model.data.operation import PostprocessingOperation
-from seqgra.model.data.rule import Rule
 from seqgra.model.data.sequenceelement import SequenceElement
 from seqgra.model.data.sequenceelement import KmerBasedSequenceElement
 from seqgra.model.data.sequenceelement import MatrixBasedSequenceElement
 from seqgra.model.data.spacingconstraint import SpacingConstraint
+from seqgra.model.data.rule import Rule
+from seqgra.model.data.operation import PostprocessingOperation
+from seqgra.model.data.condition import Condition
+from seqgra.model.data.alphabetdistribution import AlphabetDistribution
+from seqgra.model.data.datageneration import DataGeneration
+from seqgra.model.data.datageneration import ExampleSet, Example
+from seqgra.model.data.background import Background
