433f29837c8d32a27ab4d0884a4cdd86d0b23153
Konstantin Krismer <krismer@mit.edu>
2020-05-22 12:05:05 -0400

    Add basic Bayes optimal classifier
    Only supports multi-class models,
    and only one randomly placed PWM per condition,
    disregards interactions, spacing constraints, positioning, k-mers

--- seqgra/learner/bayes/bayeshelper.py
+++ seqgra/learner/bayes/bayeshelper.py
@@ -8,13 +8,16 @@ import logging
 import os
 import random
 import sys
-from typing import Any, List
+from typing import Any, Dict, List
 
 import numpy as np
 import pkg_resources
 
+import seqgra.constants as c
 from seqgra.learner import Learner
 from seqgra.model import DataDefinition
+from seqgra.model.data import Condition
+from seqgra.model.data import SequenceElement
 from seqgra.model.model import Architecture
 from seqgra.parser import DataDefinitionParser
 from seqgra.parser import XMLDataDefinitionParser
@@ -22,6 +25,44 @@ from seqgra.parser import XMLDataDefinitionParser
 
 class BayesOptimalHelper:
     @staticmethod
+    def ppm_to_pwm(ppm, alphabet_size) -> Any:
+        return np.log2(ppm * alphabet_size)
+
+    @staticmethod
+    def se_to_pwm(sequence_element: SequenceElement) -> Any:
+        # only MatrixBasedSequenceElement supported
+        alphabet_size: int = len(sequence_element.positions[0])
+        width: int = sequence_element.get_max_length()
+        ppm = np.zeros((width, alphabet_size))
+
+        if alphabet_size == 4:
+            for i, position in enumerate(sequence_element.positions):
+                total_probability = sum([letter.probability
+                                         for letter in position])
+                for letter in position:
+                    index: int = -1
+                    if letter.token.upper() == "A":
+                        index = 0
+                    elif letter.token.upper() == "C":
+                        index = 1
+                    elif letter.token.upper() == "G":
+                        index = 2
+                    elif letter.token.upper() == "T":
+                        index = 3
+                    else:
+                        raise Exception("unsupported letter: " +
+                                        letter.token.upper())
+
+                    ppm[i, index] = letter.probability / total_probability
+
+        elif alphabet_size == 20:
+            pass
+        else:
+            raise Exception("unsupported alphabet")
+
+        return BayesOptimalHelper.ppm_to_pwm(ppm, alphabet_size)
+
+    @ staticmethod
     def create_model(learner: Learner) -> None:
         arch: Architecture = learner.definition.architecture
         if arch.external_model_format != "data-definition":
@@ -34,9 +75,11 @@ class BayesOptimalHelper:
             data_def_parser: DataDefinitionParser = XMLDataDefinitionParser(
                 data_config)
 
-            data_definition: DataDefinition = data_def_parser.get_data_definition()
+            data_definition: DataDefinition = \
+                data_def_parser.get_data_definition()
             learner.model = (data_definition.conditions,
-                             data_definition.sequence_elements)
+                             BayesOptimalHelper.create_se_pwm_dict(
+                                 data_definition.sequence_elements))
         else:
             raise Exception("data definition file does not exist: " + path)
 
@@ -73,6 +116,33 @@ class BayesOptimalHelper:
         pass
 
     @staticmethod
+    def score_example(example, pwm) -> Any:
+        pwm_width: int = pwm.shape[0]
+
+        return [np.sum(np.multiply(example[i:(i + pwm_width), :], pwm))
+                for i in range(example.shape[0] - pwm_width + 1)]
+
+    @staticmethod
+    def create_se_pwm_dict(sequence_elements: List[SequenceElement]) -> Dict[str, Any]:
+        pwm_dict: Dict[str, Any] = dict()
+        for se in sequence_elements:
+            pwm_dict[se.sid] = BayesOptimalHelper.se_to_pwm(se)
+
+        return pwm_dict
+
+    @staticmethod
+    def get_pwms_for_label(label: str, conditions: List[Condition],
+                           pwm_dict: Dict[str, Any]) -> List[Any]:
+        condition: Condition = Condition.get_by_id(conditions, label)
+        pwms: List[Any] = list()
+        if condition:
+            for rule in condition.grammar:
+                for se in rule.sequence_elements:
+                    pwms.append(pwm_dict[se.sid])
+
+        return pwms
+
+    @staticmethod
     def predict(learner: Learner, x: Any, encode: bool = True):
         """ This is the forward calculation from x to y
         Returns:
@@ -81,7 +151,23 @@ class BayesOptimalHelper:
         if encode:
             x = learner.encode_x(x)
 
-        return 0.5
+        y_hat = np.zeros((x.shape[0], len(learner.definition.labels)))
+        for example_index in range(x.shape[0]):
+            for i, label in enumerate(learner.definition.labels):
+                # get rules for label
+                # for now: just PWM
+                pwms = BayesOptimalHelper.get_pwms_for_label(label,
+                                                             learner.model[0],
+                                                             learner.model[1])
+                for pwm in pwms:
+                    y_hat[example_index, i] = \
+                        max(BayesOptimalHelper.score_example(
+                            x[example_index, :, :], pwm))
+
+        if learner.definition.task == c.TaskType.MULTI_CLASS_CLASSIFICATION:
+            y_hat = y_hat / y_hat.sum(axis=1)[:, None]
+        
+        return y_hat
 
     @staticmethod
     def evaluate_model(learner: Learner, x: List[str], y: List[str]):
