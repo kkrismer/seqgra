e89c5e56e690d13be87f1c47f50dc476f50b9013
Konstantin Krismer <krismer@mit.edu>
2020-07-23 04:47:31 -0400

    Only print progress bar for more than 10 examples

--- seqgra/learner/bayes/bayeshelper.py
+++ seqgra/learner/bayes/bayeshelper.py
@@ -179,7 +179,8 @@ class BayesOptimalHelper:
                     y_hat[example_index, i] = \
                         BayesOptimalHelper.normalize_pwm_score(raw_score, pwm)
 
-            MiscHelper.print_progress_bar(example_index, x.shape[0] - 1)
+            if x.shape[0] > 10:
+                MiscHelper.print_progress_bar(example_index, x.shape[0] - 1)
 
         if learner.definition.task == c.TaskType.MULTI_CLASS_CLASSIFICATION:
             # shift
