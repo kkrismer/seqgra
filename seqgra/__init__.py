d22af6a0edf5b67584c11e7013ef0dc3c9aaec60
Konstantin Krismer <krismer@mit.edu>
2021-06-16 15:02:47 -0400

    Bump version from 0.0.3 to 0.0.4

--- seqgra/__init__.py
+++ seqgra/__init__.py
@@ -21,4 +21,4 @@ from seqgra.schema import Metrics
 from seqgra.schema import ModelSize
 from seqgra.schema import ProbabilisticToken
 from seqgra.misc import MiscHelper
-__version__ = "0.0.3"
+__version__ = "0.0.4"
